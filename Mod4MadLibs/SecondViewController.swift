ee2d6edd809e876734edf07eae3306548975b337
Jaylan <prosnipa1221@gmail.com>
2021-10-18 07:58:41 -0500

    Final Commit

--- Mod4MadLibs/SecondViewController.swift
+++ Mod4MadLibs/SecondViewController.swift
@@ -8,24 +8,16 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-    @IBOutlet weak var `return`: UILabel!
+    @IBOutlet weak var ansLab: UILabel!
+    var inc : String!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        ansLab.text = inc
         // Do any additional setup after loading the view.
     }
-
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
 }
--- Mod4MadLibs/ViewController.swift
+++ Mod4MadLibs/ViewController.swift
@@ -8,19 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var textInput:UITextField!
-    var completed = ""
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var text3: UITextField!
     
+    func emptyboxes(){
+        let alert = UIAlertController(title: "Error", message: "Please fill all boxes", preferredStyle: .alert)
+        alert.addAction(UIAlertAction (title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor.cyan
-    }
-    @IBAction func submitBut(_ sender: Any) {
-        completed = "My Uncle wants to go to the + \(textInput.text!)"
-    }
     
+   
+       
+    //  completed = "My Uncle wants to go to the + \(textInput.text!)"
+        
+    }
+        
+    @IBAction func subBut(_ sender: Any) {
+        let noun = text1.text
+        let adj = text2.text
+        let verb = text3.text
+        if noun != ""{
+            if adj != ""{
+                if verb != ""{
+                performSegue(withIdentifier: "toCompleted", sender: nil)
+                }
+                else{
+                    emptyboxes()
+                }
+            }
+            else {
+                emptyboxes()
+            }
+        }
+        else {
+            emptyboxes()
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc = segue.destination as! SecondViewController
+        nvc.inc = "My Cousin has a \(text1.text!) and a \(text2.text!) dog that he \(text3.text!)"
+    
+    
+    }
 
 }
 
